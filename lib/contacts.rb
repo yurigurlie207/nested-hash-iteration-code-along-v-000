5f23f5ee6ab704e8c7088ab8712c259fa78cda20
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-15 20:04:28 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    contacts_details_hash.each do |attribute, data|
+    contact_details_hash.each do |attribute, data|
       binding.pry
     end
   end
