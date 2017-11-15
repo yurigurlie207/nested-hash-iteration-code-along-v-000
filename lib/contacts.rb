1f055eb6ca804dbf2a6b8f6a2bbdbee074c339e5
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-15 20:29:07 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,7 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  
+
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
@@ -23,5 +23,5 @@ def remove_strawberry(contacts)
       end
     end
   end
-  
+
 end
